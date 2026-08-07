50fb49f86142071750c19839ca54bd5b31c77ff3
nowracht-afk <nowracht@gmail.com>
2026-08-07 13:38:38 +0700

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 const SUPABASE_URL = "https://hmtouplvupcenddsuqrn.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_6oR8v3-qJLVVwqQy5LkEtw_0RqfzBTA";
+sb_publishable_t4l1UIQeMqSAFuAPlgwmHQ_npBPpLW5
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 export const MEDIA_BUCKET = "media";
